b0735b494ee8d8facb2f5709a43a9799d43b585b
Amit Kashinath Birajdar <amit1711040@gmail.com>
2025-01-03 23:40:12 +0530

    all files are added

---
+++ 2_CourseIntroduction/Intro.java
@@ -0,0 +1,8 @@
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Alpha!");
+        System.out.println("This is the first course in the Alpha Program.");
+        System.out.println("We will be learning Java programming.");
+        
+    }
+}
---
+++ 3_PrerequistsInstallationResources/Prereq.java
@@ -0,0 +1,5 @@
+public class Prereq {
+    public static void main(String[] args) {
+        System.out.println("No prerequisites required it is designed for beginners only");
+    }
+}
