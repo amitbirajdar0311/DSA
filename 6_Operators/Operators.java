ca72e0e1fddefc4646bb063761b2481e387f0082
Amit Kashinath Birajdar <amit1711040@gmail.com>
2025-01-04 00:13:26 +0530

    java basics

---
+++ 6_Operators/Operators.java
@@ -0,0 +1,5 @@
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println("Operators in Java : " + "\n" + "1. Arithmetic Operators" + "\n" + "2. Relational Operators" + "\n" + "3. Logical Operators" + "\n" + "4. Bitwise Operators" + "\n" + "5. Assignment Operators" + "\n" + "6. Conditional Operators" + "\n" + "7. Unary Operators" + "\n" + "8. Shift Operators");
+    }
+}
