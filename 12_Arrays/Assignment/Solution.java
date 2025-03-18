56e72ac465b2330e5123e35435c15d5ae51c6250
Amit Kashinath Birajdar <amit1711040@gmail.com>
2025-03-18 23:53:48 +0530

    Solution file added

---
+++ 12_Arrays/Assignment/Solution.java
@@ -0,0 +1,20 @@
+
+
+public class Solution {
+    public static boolean containsDuplicate(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+        for (int j = i + 1; j < arr.length; j++) {
+            if (arr[i] == arr[j]) {
+            return true;
+            }
+        }
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 1 };
+        boolean containsDuplicate = containsDuplicate(arr);
+        System.out.println("Contains Duplicate: " + containsDuplicate);
+    }
+}
