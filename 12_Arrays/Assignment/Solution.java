f992ced8c5dbdde9cf574d21a15ea67c06e24953
Amit Kashinath Birajdar <amit1711040@gmail.com>
2025-03-19 23:37:49 +0530

    solution file minor chnages

--- 12_Arrays/Assignment/Solution.java
+++ 12_Arrays/Assignment/Solution.java
@@ -1,11 +1,14 @@
-public class Solution {
-
-  public static void main(String[] args) {
-    int[] arr = { 1, 2, 3, 4, 5 };
-    int sum = 0;
-    for (int i = 0; i < arr.length; i++) {
-      sum += arr[i];
+public class Solution{
+  public static void main(String[] args){
+    int[] arr = {-1,0,1,2,-1,-4};
+    for(int i=0;i<arr.length;i++){
+      for(int j=i+1;j<arr.length;j++){
+        for(int k=j+1;k<arr.length;k++){
+          if(arr[i]+arr[j]+arr[k]==0){
+            System.out.println(arr[i]+" "+arr[j]+" "+arr[k]);
+          }
+        }
+      }
     }
-    System.out.println(sum);
   }
 }
